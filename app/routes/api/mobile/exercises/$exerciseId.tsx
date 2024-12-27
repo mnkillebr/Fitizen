51487d3afabf2ed24fc61983da3e383fa8fbb4e9
mnkillebr <killebrew.biz@gmail.com>
2024-12-27 12:17:55 -0500

    add exercise detail mobile route

--- app/routes/api/mobile/exercises/$exerciseId.tsx
+++ app/routes/api/mobile/exercises/$exerciseId.tsx
@@ -1,8 +1,16 @@
 import { json, LoaderFunctionArgs } from "@remix-run/node";
 import { getExercise } from "~/models/exercise.server";
+import { generateMuxThumbnailToken, generateMuxVideoToken } from "~/mux-tokens.server";
 
 export async function loader({ params }: LoaderFunctionArgs) {
   const exerciseId = params.exerciseId as string;
   const exercise = await getExercise(exerciseId)
-  return json(exercise);
+  const videoToken = generateMuxVideoToken(exercise.muxPlaybackId)
+  const thumbnailToken = generateMuxThumbnailToken(exercise.muxPlaybackId, undefined, undefined)
+  const videoMappedExercise = {
+      ...exercise,
+      videoToken,
+      thumbnail: thumbnailToken ? `https://image.mux.com/${exercise.muxPlaybackId}/thumbnail.png?token=${thumbnailToken}` : undefined,
+    }
+  return json(videoMappedExercise);
 };
